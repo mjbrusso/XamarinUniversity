7e1608face11be9453e2f2321d2c170ca0ae1f99
mjbrusso <mjbrusso@gmail.com>
2018-02-12 17:15:23 -0200

    Add xam120-FirstApp exercise

---
+++ xam120-FirstApp/xam120-FirstApp/xam120-FirstApp/MainPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace xam120_FirstApp
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+		}
+	}
+}
